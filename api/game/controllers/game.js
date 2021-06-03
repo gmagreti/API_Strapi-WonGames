41cff835b30ce2e2514768cbe295e0d19efd7a58
Gabriel Magreti <gabrielmagreti@gmail.com>
2021-06-03 11:36:10 -0300

    Buscando dados na com JSDOM

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,14 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  populate: async (ctx) => {
+    ctx.send({ ok: true });
+
+    await strapi.services.game.populate();
+  },
+};
